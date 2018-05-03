ddc19e89c779c0d71e64c5771ec94fdb6f452676
Laurent Fasnacht <l@libres.ch>
2018-05-03 17:23:17 +0200

    New setup.py, __version__

--- multiregression/__init__.py
+++ multiregression/__init__.py
@@ -1,2 +1,3 @@
 from .expreval import ExprEvaluator, RegrExprEvaluator
 from .multiregression import Multiregression, ModelMultiregression
+from ._version import __version__
--- setup.py
+++ setup.py
@@ -1,13 +1,64 @@
-from setuptools import setup
-setup(
-    name='multiregression',
-    description='Python 3 module for doing simultaneous linear regression', 
-    version='1.0.0',
-    packages=['multiregression'],
-    author="Laurent Fasnacht",
-    author_email="l@libres.ch", 
-    url = 'https://github.com/UniNE-CHYN/multiregression',
-    install_requires=[
-        'numpy',
-    ],    
-)
+from setuptools import setup, find_packages
+import re
+import os
+
+version_data = open("multiregression/_version.py", "r").read()
+mo = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]", version_data, re.M)
+
+if mo:
+    version_string = mo.group(1)
+else:
+    raise RuntimeError("Unable to find version string")
+
+long_description = open('README.md', 'r').read()
+
+
+def test_suite():
+    import unittest
+    test_loader = unittest.TestLoader()
+    test_suite = test_loader.discover('tests', pattern='test_*.py')
+    return test_suite
+
+
+platform_requirements = []
+
+_setup_data = {
+    # Base information
+    'name': 'multiregression',
+    'version': version_string,
+    'packages': find_packages(),
+    'test_suite': 'setup.test_suite',
+    'tests_require': ['numpy'] + platform_requirements,
+
+    # Description and classification
+    'description': 'Python 3 module for doing simultaneous linear regression.',
+
+    'long_description': long_description,
+    'long_description_content_type': 'text/markdown',
+    'classifiers': [
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'Development Status :: 5 - Production/Stable',
+        'License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)'
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6'
+    ],
+
+    # Author and license...
+    'author': "Laurent Fasnacht",
+    'author_email': "l@libres.ch",
+    'maintainer': "Laurent Fasnacht",
+    'maintainer_email': "l@libres.ch",
+    'url': 'https://github.com/UniNE-CHYN/multiregression',
+    'license': 'LGPLv3',
+
+    # Requirements
+    'python_requires': '>=3.4',
+    'install_requires': ['numpy'] + platform_requirements,
+}
+
+if __name__ == '__main__':
+    setup(**_setup_data)
